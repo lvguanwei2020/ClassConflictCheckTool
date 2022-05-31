114dc6b9f5be7dbd0a76a6cefc5ca22734b709b8
GitHub Action <action@github.com>
2022-05-31 01:50:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lvguanwei2020/classconflictchecktool/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lvguanwei2020.classconflictchecktool.services
+
+import com.github.lvguanwei2020.classconflictchecktool.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
